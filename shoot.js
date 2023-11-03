6e7e8db2c206c68e2f4067e4de22b5dfa5059632
Ophilia Fradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2023-11-03 16:40:02 +0530

    Add files via upload

---
+++ shoot.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent("bullets", {
+  init: function () {
+    this.shootBullet();
+  },
+  shootBullet: function () {
+ 
+  },
+  removeBullet: function (e) {
+  
+  },
+  shootSound: function () {
+   
+  },
+});
+
